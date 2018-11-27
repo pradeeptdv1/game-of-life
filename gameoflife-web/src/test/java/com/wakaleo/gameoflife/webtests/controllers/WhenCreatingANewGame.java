513470498f0f12386df18a151e42f0b238b6832a
pradeeptdv1 <36186110+pradeeptdv1@users.noreply.github.com>
2018-11-27 13:07:29 +0530

    Update WhenCreatingANewGame.java

--- gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
+++ gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
@@ -40,7 +40,7 @@ public class WhenCreatingANewGame {
         HttpServletRequest request = mock(HttpServletRequest.class);
         ModelAndView homeView = controller.firstGeneration(3, 5, request);
         assertThat((Integer) homeView.getModel().get("rows"), is(3));
-        assertThat((Integer) homeView.getModel().get("columns"), is(5));
+        assertThat((Integer) homView.getModel().get("columns"), is(5));
 
     }
 }	

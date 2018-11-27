d9bafcbfc0d4be0f668471a751364d90a124e0b0
pradeeptdv1 <36186110+pradeeptdv1@users.noreply.github.com>
2018-11-27 14:23:01 +0530

    Update HomePageController.java

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomePageController {
     @RequestMapping("/")
     public ModelAndView index() {
-        return new ModelAndView("home");
+        return new ModelAndView("hom");
     }
 
 	// Clicking the "home" text loads the main page
